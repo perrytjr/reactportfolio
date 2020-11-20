d396a2c47df1b160c194335597f9b3980df9e0e3
Perry Theobald <perrytjr@gmail.com>
2020-11-20 10:11:32 -0800

    added data to data.js enought to start rendering something on pages to work with

---
+++ src/components/data.js
@@ -0,0 +1,95 @@
+export default {
+   
+  
+   
+    name: "Perry Theobald.",
+  
+    //Contact Email
+    contactEmail: "perrytjr@gmail.com",
+  
+    // Landing Page Name
+    landingPageName: "Perry Theobald",
+  
+    // Landing Page Paragraph
+    landingPagePara:
+      "I'm a San Diego based Full Stack Developer. I enjoy working throughout the stack but I'm leaning more towards specializing in backend development. ",
+  
+    // Landing Page Image (replace - root/public/assets/photo.jpg)
+    landingPageImage: "assets/photo.jpg",
+  
+    // Change Projects Here
+    projects: [
+      {
+        id: 1, 
+        title: "Weather Dashboard", 
+        // Image Size Should be (300X300 px)
+        imageSrc: "https://via.placeholder.com/300",
+        //Project URL - Add Your Project Url Here
+        url: "https://github.com/perrytjr/Weather-Dashboard"
+      },
+      {
+        id: 2, 
+        title: "Password Generator",
+        imageSrc: "https://via.placeholder.com/300",
+        url: "https://github.com/perrytjr/PasswordGenerator"
+      },
+      {
+        id: 3, 
+        title: "Quarantine Day Planner",
+        imageSrc: "https://via.placeholder.com/300",
+        url: "https://github.com/perrytjr/project1"
+      },
+      {
+        id: 4, 
+        title: "Boo Chat",
+        imageSrc: "https://via.placeholder.com/300",
+        url: "https://github.com/perrytjr/project2"
+      },
+      {
+        id: 5, 
+        title: "Eat-Da-Burger",
+        imageSrc: "https://via.placeholder.com/300",
+        url: "https://github.com/perrytjr/Eat-Da-Burger"
+      },
+      {
+        id: 6, 
+        title: "Team Generator",
+        imageSrc: "https://via.placeholder.com/300",
+        url: "https://github.com/perrytjr/Employee-Team-Generator"
+      }
+      // Paste more projects here from the template
+  
+      /*
+                      If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                  ,{
+                  id: 7,
+                  service: 'Something Amazing',
+                  imageSrc: "",
+                  url: ''
+              }
+                  */
+    ],
+  
+    // Contact Section
+    contactPara:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    social: [
+      
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/perry-theobald-42450241/"
+      },
+      {
+        name: "Github",
+        url: "https://github.com/perrytjr"
+      },
+      {
+        name: "Google Drive",
+        url: "https://drive.google.com/file/d/1M8y3onVyfxiIhB6zC2pYxxpBTXTK6Qfc/view?usp=sharing"
+      }
+    ],
+  
+    // About Page
+    aboutTitle: "I make remarkable digital products",
+    aboutPara:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy text eversince the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  };
